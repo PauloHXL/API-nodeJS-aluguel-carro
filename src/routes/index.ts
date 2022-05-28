e0ce622841807e82d738c7bfdce4bdb217bf6969
Paulo Henrique <paulo.xl2009@gmail.com>
2022-05-27 21:20:56 -0300

    correção rota

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { categoriesRoutes } from "./categories.routes"
-import { specificationRoutes } from "./spesification.routes"
+import { specificationRoutes } from "./specification.routes"
 
 
 
